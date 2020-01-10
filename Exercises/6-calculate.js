a088a079005f8396c0189db040bec74a2b853c0d
newbornfrontender <newbornfrontender@gmail.com>
2020-01-10 13:36:50 +0300

    feat(6): finish ex

--- Exercises/6-calculate.js
+++ Exercises/6-calculate.js
@@ -1,11 +1,29 @@
 'use strict';
 
-const square = null;
+const square = n => n * n;
 
-const cube = null;
+const cube = n => n ** 3;
 
-const average = null;
+const average = (a, b) => (a + b) / 2;
 
-const calculate = null;
+// const average = (...nums) => {
+//   const length = nums.length;
+//   const sum = nums.reduce((acc, current) => acc + current);
+//   const result = sum / length;
+
+//   return result;
+// };
+
+const calculate = () => {
+  const arr = [];
+
+  for (let i = 0; i <= 9; i++) {
+    const result = average(square(i), cube(i));
+
+    arr.push(result);
+  }
+
+  return arr;
+};
 
 module.exports = { square, cube, average, calculate };
