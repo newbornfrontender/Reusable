8d4b342fff698e1fe5a3cce995a77ae4d99e37ea
newbornfrontender <newbornfrontender@gmail.com>
2020-01-10 13:03:35 +0300

    feat(4-5): finish ex

--- Exercises/4-range.js
+++ Exercises/4-range.js
@@ -1,5 +1,11 @@
 'use strict';
 
-const range = null;
+const range = (start, end) => {
+  const nums = [];
+
+  for (let i = start; i <= end; i++) nums.push(i);
+
+  return nums;
+};
 
 module.exports = { range };
--- Exercises/5-range-odd.js
+++ Exercises/5-range-odd.js
@@ -1,5 +1,13 @@
 'use strict';
 
-const rangeOdd = null;
+const rangeOdd = (start, end) => {
+  const nums = [];
+
+  for (let i = start; i <= end; i++) {
+    if (i % 2 !== 0) nums.push(i);
+  }
+
+  return nums;
+};
 
 module.exports = { rangeOdd };
