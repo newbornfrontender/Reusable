56ebe261cb452a86a204dafe91eab74ae59efa9c
newbornfrontender <newbornfrontender@gmail.com>
2020-01-13 10:45:37 +0300

    feat(9-hash): complete ex

--- Exercises/9-array.js
+++ Exercises/9-array.js
@@ -1,7 +1,32 @@
 'use strict';
 
-const phonebook = null;
+const phonebook = [
+  {
+    name: 'Alex',
+    phone: '+79159000000',
+  },
+  {
+    name: 'Marcus',
+    phone: '+17143000000',
+  },
+  {
+    name: 'Fred',
+    phone: '+77143111111',
+  },
+  {
+    name: 'Ann',
+    phone: '+81253002201',
+  },
+  {
+    name: 'Hayako',
+    phone: '+61253033201',
+  },
+];
 
-const findPhoneByName = null;
+const findPhoneByName = name => {
+  for (const user of phonebook) {
+    if (user.name === name) return user.phone;
+  }
+};
 
 module.exports = { phonebook, findPhoneByName };
--- Exercises/a-hash.js
+++ Exercises/a-hash.js
@@ -1,7 +1,13 @@
 'use strict';
 
-const phonebook = null;
+const phonebook = {
+  Alex: '+79159000000',
+  Marcus: '+17143000000',
+  Fred: '+77143111111',
+  Ann: '+81253002201',
+  Hayako: '+61253033201',
+};
 
-const findPhoneByName = null;
+const findPhoneByName = name => phonebook[name];
 
 module.exports = { phonebook, findPhoneByName };
