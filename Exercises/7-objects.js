0173524daa8386d477d9061f019b07cd8c069f7b
newbornfrontender <newbornfrontender@gmail.com>
2020-01-13 10:12:11 +0300

    feat(7-8): complete ex

--- Exercises/7-objects.js
+++ Exercises/7-objects.js
@@ -1,5 +1,27 @@
 'use strict';
 
-const fn = null;
+const fn = () => {
+  const obj1 = {
+    name: 'Tom',
+  };
+
+  let obj2 = {
+    name: 'Jerry',
+  };
+
+  obj1.name = 'Jerry';
+  obj2.name = 'Tom';
+
+  obj2 = {
+    name: 'Bob',
+  };
+};
+
+/* Объяснение:
+ *
+ * В поля передается скалярное значение.
+ *
+ * Изменить obj1 нельзя, т.к. невозможно присвоить константе новое значение.
+ */
 
 module.exports = { fn };
